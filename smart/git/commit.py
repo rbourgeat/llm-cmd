df2f18518000dedb2336c0d6ebb56e5a8fdf098b
krolhm <raphaelb1@icloud.com>
2023-10-03 23:02:41 +0200

    Add subproject llama.cpp and commit changes in smart/git/commit.py

---
+++ smart/git/commit.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import re
+import json
+import requests
+import subprocess
+
+def main():
+    git_diff = subprocess.check_output(['git', 'diff', '--cached']).decode('utf-8')
+
+    changed_lines = [line.strip() for line in git_diff.split('\n') if line.startswith('+')]
+
+    if not changed_lines:
+        print("No changes staged for commit.")
+        return
+
+    commit_message = '\n'.join(changed_lines)
+
+    template = f"<s>[INST] You are a developer who is working\
+        on a project and wants to push his work to a git repository.\
+        Write a commit message, this is the git diff --cached result:\
+        ```{commit_message}```[/INST]</s>"
+
+    url = "http://localhost:8080/completion"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "prompt": template,
+        "n_predict": 150
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        response_data = json.loads(response.text)
+
+        if (response_data['content'] == ""):
+            main()
+            return
+
+        commit_message = response_data['content'].replace('\n', '')
+        commit_message = commit_message.replace('<s>', '')
+        commit_message = commit_message.replace('</s>', '')
+        commit_message = commit_message.replace('"', '')
+        
+        # subprocess.call(['git', 'commit', '-m', cleaned_message])
+
+        print(f'git commit -m "{commit_message}"')
+
+    else:
+        print(f"Error: Request failed with status code {response.status_code}")
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
